702321f7727642ebe282880f57c04b69daf565cb
evanpaul22 <evanpaul22@gmail.com>
2015-11-19 21:13:28 -0800

    Deliverable

--- quiz.js
+++ quiz.js
@@ -3,15 +3,14 @@ function qAndA(question,answer)
 {
 	this.question = question;
 	this.answer = answer;
-}	
+}
 
-var questions = 
+var questions =
 [
 	new qAndA("A society whose processes/function can continue forever","Sustainable Society"),
 	new qAndA("Resources that can be replenished at rate greater than or equal to the rate at which they are used","Renewable"),
 	new qAndA("Development of communities in a previously lifeless area without soil","Primary Succession"),
 	new qAndA("Organization of living things that consists of all living and nonliving things in the area as well as interactions between them","Ecosystem"),
-	new qAndA("A marketing term for practices that suggest sustainability when its not","Greenwashing"),
 	new qAndA("Breaking down the original product by some physical or chemical means, retrieving the materials, and using them as raw materials to make new products","Recycle"),
 	new qAndA("The amount of resources that can be removed/harvested with out compromising ecosystem","Sustainable Yield")
 ];
@@ -21,52 +20,81 @@ var score = 0;
 var totalQuestions;
 var remainingQuestions = totalQuestions = questions.length;
 var questionsAnswered = 0;
-		
+var selected;
 //randomly chooses a question that has not been chosen before
 function selectQuestion()
 {
 	//end case
 	if(remainingQuestions == 0)
 	{
-		alert("You have finished this quiz! \nYour total score is " + score + "/"+ totalQuestions+ "\nYou can refresh the page to try again.");
+		$("#question").text("You have finished this quiz! Your total score is " + score + "/"+ totalQuestions+ ".You can refresh the page to try again.")
 		return;
-	}	
+	}
 	var randomIndex = Math.floor(Math.random() * remainingQuestions);
 	currentIndex = randomIndex;
 	var randomQuestion = questions[randomIndex].question;
-	
-	//fill in the value on the page
-	document.getElementById("question").value = randomQuestion;
-}
 
+	//fill in the value on the page
+	$("#question").text(randomQuestion);
+}
 //check the provided answer with the correct one
-function checkAnswer()
+function checkAnswer(item)
 {
-	var enteredAnswer = document.getElementById("answer").value;	
+	var enteredAnswer = item.text();
 	var correctAnswer = questions[currentIndex].answer;
-			
-	//no answer given
-	if(enteredAnswer == "")
-	{
-		alert("Please enter an answer before checking.");
-		return;
-	}
-	questionsAnswered +=1;	
-	
-	//answer is correct
+
+	questionsAnswered +=1;
+
 	if(enteredAnswer == correctAnswer)
 	{
-		score+=1;
-		alert("Correct! Your score is: " + score + "/" + questionsAnswered);
-	}
-			
-	//answer is incorrect
-	else
+		$("#score").text(++score);
+		$("#question-container-body").css("background-color", "lightgreen");
+		item.css("visibility", "hidden"); // 'Delete' the answer (without affecting DOM)
+	}else
 	{
-		alert("Incorrect! The answer was " + correctAnswer + " \nYour score is: " + score + "/" + questionsAnswered);
+		$("#question-container-body").css("background-color", "lightcoral");
 	}
-			
-	//remove current country from the array so it is not repeated
+	//remove current question from the array so it is not repeated
 	questions.splice(currentIndex,1);
 	remainingQuestions-=1;
-}	
+}
+
+$(document).ready(function(){
+	// Set date
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var year = date.getFullYear();
+	$("#date").text(month+"/"+day+"/"+year);
+
+	selectQuestion();
+	// Stop Quiz
+	$("#stop").on("click", function(){
+		remainingQuestions = 0;
+		selectQuestion();
+		$( ".choice" ).draggable( "disable" );
+	});
+	// Next question
+	$("#next").on("click", function(){
+		selectQuestion();
+		$( ".choice" ).draggable( "enable" );
+		$("#question-container-body").css("background-color", "initial");
+	});
+	// Drag
+	$(".choice").draggable({
+		snap: "#target-container",
+		snapMode: "inner",
+		snapTolerance: 50,
+		opacity: .5,
+		revert: "valid"
+	});
+	// Drop
+	$( "#target-container" ).droppable({
+  	accept: ".choice",
+		tolerance: "fit",
+		drop: function(e, ui){
+			checkAnswer(ui.draggable);
+			$( ".choice" ).draggable( "disable" ); // Don't allow dragging more answers until new question
+		}
+	});
+});
